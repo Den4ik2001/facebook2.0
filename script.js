eb94686b288687f11afefb609c494e6859db7b1c
Den4ik2001 <58068616+Den4ik2001@users.noreply.github.com>
2020-08-30 14:24:23 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let burger_menu = document.querySelector(".burger-menu");
 
 function burg() {
-	if (burger_menu.style.display === 'none') {
-		burger_menu.style.display = 'block';
+	if (burger_menu.style.display === 'block') {
+		burger_menu.style.display = 'none';
 	}
 	else {
-		burger_menu.style.display = 'none';
+		burger_menu.style.display = 'block';
 	}
 }
